910628a2d9a5b5d99f783c130426f258b4c03d28
dtucker-redhat <dtucker@redhat.com>
2015-01-23 16:08:53 -0500

    Adding new method for discovery
    Added discovery via JMX since when I tested JGroups it wasn't working
    straight away

--- src/main/java/com/redhat/ClusterIdentifier.java
+++ src/main/java/com/redhat/ClusterIdentifier.java
@@ -1,7 +1,15 @@
 package com.redhat;
 
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
 
 import org.jboss.as.server.CurrentServiceContainer;
 import org.jboss.msc.service.ServiceName;
@@ -14,8 +22,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ClusterIdentifier {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(ClusterIdentifier.class);
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(ClusterIdentifier.class);
 
 	public List<IpAddress> getJGroupsMembers() {
 		Channel channel = (Channel) CurrentServiceContainer
@@ -31,11 +40,18 @@ public class ClusterIdentifier {
 			PhysicalAddress physicalAddr = (PhysicalAddress) channel
 					.down(new Event(Event.GET_PHYSICAL_ADDRESS, member));
 			IpAddress ipAddr = (IpAddress) physicalAddr;
-			LOG.debug("Found IP: "
-					+ ipAddr.getIpAddress().getHostAddress());
+			LOG.debug("Found IP: " + ipAddr.getIpAddress().getHostAddress());
 			addresses.add(ipAddr);
 		}
-		
+
 		return addresses;
+	}
+
+	public void getJGroupsViaJmx() throws AttributeNotFoundException, InstanceNotFoundException, MalformedObjectNameException, MBeanException, ReflectionException, NullPointerException {
+		Object obj = ManagementFactory.getPlatformMBeanServer().getAttribute(
+				ObjectName.getInstance("jgroups:type=channel,cluster=\"web\""),
+				"View");
+		
+		System.out.println(obj);
 	}
 }
